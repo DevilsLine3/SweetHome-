db13a28e413c34e1b7fe316cab204ae757da3b30
DevilsLine3 <justingalvis10@hotmail.com>
2025-11-12 14:48:07 -0500

    cambio para despliegue

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,4 @@
-import'dotenv/config';
+import 'dotenv/config';
 import express from 'express';  
 import cors from 'cors';
 import bodyParser from 'body-parser';
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || '*',
     credentials: true
 }));
 
